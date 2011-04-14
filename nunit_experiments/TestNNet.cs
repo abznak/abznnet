2a16074c7dc9d10a3ce6b197052bb75ce9a048e9
Tim Smith <code@abznak.com>
2011-04-14 12:47:28 +1000

    improvements to nnet

---
+++ nunit_experiments/TestNNet.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tims
+ * Date: 2011-04-13
+ * Time: 2:24 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace com.abznak.nnet
+{
+	
+	[TestFixture]
+	public class TestNNet
+	{
+		[Test]
+		public void TestAF_ID() {
+			double[] ds = new double[] {0, 1, 100, -100, double.MaxValue, double.MinValue};
+			foreach (double d in ds) {
+				Assert.AreEqual(d, NNet.AF_ID(d));
+			}
+		}
+		[Test]
+		public void TestAF_TANH() {
+			double[] ds = new double[] {0, 1, 100, -100, double.MaxValue, double.MinValue};
+			foreach (double d in ds) {
+				double got = NNet.AF_TANH(d);
+				Assert.AreEqual(Math.Tanh(d), got);
+				Assert.LessOrEqual(-1, got);
+				Assert.GreaterOrEqual(1, got);				
+			}
+		}		
+		
+	}
+	[TestFixture]
+	public class TestFeedForwardNNet
+	{
+
+		private FeedForwardNNet nn;
+		private int want_in, want_out;
+		private double[][][] want_weights;
+		
+
+		[Test]
+		public void TestConstruct()
+		{
+			makeSample();
+			Assert.AreEqual(want_in, nn.input_count);
+			Assert.AreEqual(want_out, nn.output_count);
+			//want_weights[1][1][1] = 15;
+			Assert.AreEqual(want_weights, nn.weights);		
+		}
+		
+		[Test]
+		public void TestAvg() {
+			makeAvgNN();
+			double a = 6, b = 10;
+			double[] output = nn.process(new double[] {a, b});
+			Assert.AreEqual(want_out, output.Length);
+			Assert.AreEqual((a + b)/2, output[0]);
+		}
+
+		public void makeSample()
+		{
+			want_in = 2;
+			want_out = 3;
+			want_weights = makeWeights();
+			nn = new FeedForwardNNet(want_in, want_out, makeWeights(), NNet.AF_ID);
+		}
+		private double[][][] makeWeights() {
+			return new double[][][] {
+				new double[][] { new double[]{5,2,3}, new double[]{1,2,3}, new double[]{1,2,3} },
+				new double[][] { new double[]{1,2,3}, new double[]{2,2,3}, new double[]{1,2,3} },
+				new double[][] { new double[]{1,2,3}, new double[]{1,2,3}, new double[]{1,2,7} },
+			};			
+			
+		}
+		
+		[Test]
+		public void makeOffsetNN() {
+			want_in = 1;
+			want_out = 1;
+			double offset = 13.5;
+			want_weights = new double[][][] {
+				new double[][] { new double[] {0, offset}}
+			};
+			nn = new FeedForwardNNet(want_in, want_out, want_weights, NNet.AF_ID);
+			double[] got = nn.process(new double[] {5});
+			Assert.AreEqual(offset, got[0]);
+		}
+		private void makeAvgNN() {
+			want_in = 2;
+			want_out = 1;
+			want_weights = new double[][][] {
+				new double[][] { new double[] {.5, .5, 0}}
+			};
+			nn = new FeedForwardNNet(want_in, want_out, want_weights, NNet.AF_ID);
+		}
+		[Test]
+		public void makeMultiLevelNN() {
+			want_in = 1;
+			want_out = 1;
+			want_weights = new double[][][] {
+				new double[][] { new double[] {2, 0} },
+				new double[][] { new double[] {3, 0} },				
+			};
+			nn = new FeedForwardNNet(want_in, want_out, want_weights, NNet.AF_ID);
+			double[] got = nn.process(new double[] {5});		
+			Assert.AreEqual(2*3*5, got[0]);
+		}
+		
+//		[TestFixture]
+//		public void Dispose()
+//		{
+//			
+//		}
+//				
+	}
+}
---
+++ nunit_experiments/nnet.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tims
+ * Date: 2011-04-13
+ * Time: 2:06 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace com.abznak.evolve {
+	public interface Evolveable<T> {
+		T makeChild(double mutationRate);
+	}	
+}
+
+namespace com.abznak.nnet
+{
+	/// <summary>
+	/// activation function is used to scale the output of a neuron 
+	/// </summary>
+	public delegate double ActivationFunction(double input);
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public abstract class NNet
+	{
+		/// <summary>
+		/// the identity activation function
+		/// </summary>
+		public static readonly ActivationFunction AF_ID = delegate(double d) {return d;};  
+		/// <summary>
+		/// tanh is a nice activation function, it rescales output to be in [-1,1]
+		/// </summary>
+		public static readonly ActivationFunction AF_TANH = delegate(double d) {return Math.Tanh(d);};
+		public abstract double[] process(double[] src);
+		private static double id(double d) { return d;}
+	}
+	
+	public class FeedForwardNNet : NNet, evolve.Evolveable<FeedForwardNNet> {
+		public int input_count {get; private set;}
+		public int output_count {get; private set;}
+		public double[][][] weights {get; private set;}
+		//public double[][] sums {get; private set;}
+		public double[][] activations {get; private set;}
+		public ActivationFunction af {get; private set;}
+
+		/// <summary>
+		/// Create feed forward, fully connected simulated Neural Network.
+		/// For efficiency and lazieness reasons, weights isn't deep copied as much as it should be. so... don't mess with it.
+		/// </summary>
+		/// <param name="input_count">number of inputs</param>
+		/// <param name="output_count">number of outputs</param>
+		/// <param name="weights">weights by layer, dst neuron, src neuron. at the end of each list of weights is an offset (i.e. a weight for a neuron that always outputs 1).</param>
+		public FeedForwardNNet(int input_count, int output_count, double[][][] weights, ActivationFunction af) {
+			this.input_count = input_count;
+			this.output_count = output_count;
+			this.weights = weights;
+			this.af = af;
+		}
+		/// <summary>
+		/// checks that the weights array is sane (i.e. the sizes of the rows is correct)
+		/// </summary>
+		/// <returns>true iff weights array is OK</returns>
+		public bool isSane() {
+			throw new Exception("NYI");
+		}
+		
+		public FeedForwardNNet makeChild(double mutationRate) {
+			return null;
+		}
+		public override double[] process(double[] src) {
+			activations = new double[weights.Length+1][];  //note that activations[0] is src, whereas weights[0] is hidden layer 1
+			activations[0] = src; //TODO: deep clone
+			
+			//li - layer index
+			//di - dst unit index
+			//si - src unit index
+			for (int li = 0; li < weights.Length; li++) {
+				activations[li+1] = new double[weights[li].Length];
+				double [][] layer = weights[li];
+				for (int di = 0; di < layer.Length; di++) {
+					double [] unit = layer[di];
+					double sum = 0;
+					int si;
+					for (si = 0; si < unit.Length - 1; si++) {
+						sum += unit[si] * activations[li][si];
+					}
+					sum += 1 * unit[si];  //offset
+					activations[li+1][di] = af(sum);
+				}
+			}
+			return activations[weights.Length];
+		}
+	}
+}
